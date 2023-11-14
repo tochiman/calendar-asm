b0ede4cd99af9e9b2a03ec6c54270d2f62e907ec
tochiman <developer@tochiman.com>
2023-11-14 10:33:33 +0900

    [Update]作業1まで完了

--- calendar.c
+++ calendar.c
@@ -2,13 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define IW          (7)
+#define CW          (3)
+#define OW          (21)
+#define OH          (6)
+#define MAX_CANVAS  (OW*OH)
+
 extern int monthwoffset(int,int,int);
 extern int monthlen(int,int);
+extern void mk1cal(int,int,char*);
 
 int main(int argc, char *argv[]){
 	int y;
 	int m;
 //	int d;
+	char canvas[MAX_CANVAS];
 
 	if (argc==3){
 		y = atoi(argv[1]);
@@ -17,5 +25,14 @@ int main(int argc, char *argv[]){
 	}else{
 		printf("usage: %s y m\t - one month\n", argv[0]);
 	}
-	printf("%d\n",monthlen(y,m));
+	memset(canvas, ' ', sizeof(canvas));
+	mk1cal(y,m,canvas);
+    for(int i=0;i<MAX_CANVAS;i++) {
+        if(canvas[i]) {
+            printf("%c", canvas[i]);
+        }
+        if(i%OW==OW-1) {
+            printf("\n");
+        }
+    }
 }
