c39b7093a744e5f74a0603c3228e06ad59802337
tochiman <developer@tochiman.com>
2023-11-15 12:03:47 +0900

    [Fix]エラー分が出ているのにカレンダーを出力しようとしている

--- calendar.c
+++ calendar.c
@@ -15,24 +15,23 @@ extern void mk1cal(int,int,char*);
 int main(int argc, char *argv[]){
 	int y;
 	int m;
-//	int d;
 	char canvas[MAX_CANVAS];
 
 	if (argc==3){
 		y = atoi(argv[1]);
 		m = atoi(argv[2]);
-//		d = atoi(argv[3]);
+
+		memset(canvas, ' ', sizeof(canvas));
+		mk1cal(y,m,canvas);
+		for(int i=0;i<MAX_CANVAS;i++) {
+			if(canvas[i]) {
+				printf("%c", canvas[i]);
+			}
+			if(i%OW==OW-1) {
+				printf("\n");
+			}
+		}
 	}else{
 		printf("usage: %s y m\t - one month\n", argv[0]);
 	}
-	memset(canvas, ' ', sizeof(canvas));
-	mk1cal(y,m,canvas);
-    for(int i=0;i<MAX_CANVAS;i++) {
-        if(canvas[i]) {
-            printf("%c", canvas[i]);
-        }
-        if(i%OW==OW-1) {
-            printf("\n");
-        }
-    }
 }
